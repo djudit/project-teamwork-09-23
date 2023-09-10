a3c70a6aa9fcc7abac5c75619d56619a5b23b82b
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-09-10 23:25:12 +0300

    Mobile menu fixing bugs

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLinks = document.querySelectorAll('.mob-menu-item');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,7 +18,9 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
+  menuLinks.forEach(item => {
+    item.addEventListener('click', toggleMenu);
+  });
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
